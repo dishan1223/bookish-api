97a3fd3f6e672ea2a7849310ee08b2fbb4b79bb5
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-07-09 10:11:56 +0600

    connected to render for database

--- types/types.go
+++ types/types.go
@@ -16,9 +16,7 @@ type Books struct {
     Description string  `json:"description" validate:"max=1000"`
     Poster      string  `json:"poster" validate:"required,url"`
     Pages       uint    `json:"pages" validate:"required,gt=0"`
-    Price       float64 `json:"price" validate:"required,gt=0"`
     Author      string  `json:"author" validate:"required,min=2,max=100"`
-    Link        string  `json:"link" validate:"url"`
     DownloadLink string  `json:"download_link" validate:"url"`
 }
 
